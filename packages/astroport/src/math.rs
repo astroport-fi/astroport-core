76f6255f4358fdb92fe37bd2fc089ad9f62327e6
The Larry <26318510+0xlarry@users.noreply.github.com>
2021-09-16 16:07:06 +0100

    Update comments

--- packages/astroport/src/math.rs
+++ packages/astroport/src/math.rs
@@ -1,8 +1,7 @@
 use cosmwasm_std::Uint128;
 
-/// Addition where if overflow happens, warp back to zero
+/// If the addition results in integer overflow, safely warps the number back to zero
 pub fn warp_add(a: Uint128, b: Uint128) -> Uint128 {
-    // Uint128::new(a.u128() + b.u128())
     // The difference between a and Uint128::MAX, i.e. biggest number that can be added to
     // a without overflowing
     let diff = Uint128::MAX - a;
