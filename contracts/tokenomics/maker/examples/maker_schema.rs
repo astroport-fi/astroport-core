fb63efe72b5721d2af77270fa2540522433e495c
IvanPavlov <ip@atticlab.net>
2021-10-19 10:27:03 +0300

    update schemas

--- contracts/tokenomics/maker/examples/maker_schema.rs
+++ contracts/tokenomics/maker/examples/maker_schema.rs
@@ -3,7 +3,7 @@ use std::fs::create_dir_all;
 
 use cosmwasm_schema::{export_schema, remove_schemas, schema_for};
 
-use astroport_maker::msg::{ExecuteMsg, InstantiateMsg, QueryConfigResponse, QueryMsg};
+use astroport::maker::{ExecuteMsg, InstantiateMsg, QueryConfigResponse, QueryMsg};
 
 fn main() {
     let mut out_dir = current_dir().unwrap();
