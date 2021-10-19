5b1216f968659ab0cc6b5c09a3ad54041e3abde2
P-Yevhenii <py@atticlab.net>
2021-10-19 19:44:39 +0300

    Added parser withdraw amounts

--- scripts/migrate_liquidity.ts
+++ scripts/migrate_liquidity.ts
@@ -6,13 +6,14 @@ const astroportFactoryAddr = "terra18qpjm4zkvqnpjpw0zn0tdr8gdzvt8au35v45xf"
 const terraSwapPairAddr = "terra18um88jh26gwq5varc570ze8m24q79q9n02sd33"
 const astroportPairAddr = "terra18um88jh26gwq5varc570ze8m24q79q9n02sd33"
 
-const chainID="localterra"
+const chainID="bombay-12"
 const nodeURL ="https://bombay-lcd.terra.dev"
 const walletMnemonic="quality vacuum heart guard buzz spike sight swarm shove special gym robust assume sudden deposit grid alcohol choice devote leader tilt noodle tide penalty"
 
+
 async function migrateLiquidity(fromPairAddr: string, toPairAddr: string, amount: string) {
-    //const cl = newClient(nodeURL, chainID, walletMnemonic);
-    const cl = newClient();
+    const cl = newClient(nodeURL, chainID, walletMnemonic);
+    //const cl = newClient();
 
     if (!await isAllowedAmountInPool(cl, fromPairAddr, amount, {"pool": {}})) {
         console.log("This amount not allowed in pool");
@@ -26,7 +27,7 @@ async function migrateLiquidity(fromPairAddr: string, toPairAddr: string, amount
         "send": {
             "contract": fromPairAddr,
             "amount": amount,
-            "msg": Buffer.from('{"withdraw_liquidity": {}}').toString('base64')
+            "msg": Buffer.from(JSON.stringify({"withdraw_liquidity": {}})).toString('base64')
         }
     });
     console.log("withdrawResp: ", withdrawResponse);
@@ -37,11 +38,14 @@ async function migrateLiquidity(fromPairAddr: string, toPairAddr: string, amount
     }
     console.log("assetInf: ", assetsInf);
 
-    let msgPL = await msgProvideLiquidity(assetsInf, withdrawResponse);
-    console.log("msgPL: ", msgPL);
+    let assetsAmount = withdrawAssetsAmount(withdrawResponse);
+    console.log("amount in withdraw: ", assetsAmount);
+
+    let msgPL = msgProvideLiquidity(assetsInf, assetsAmount);
+    console.log("msgPL: ", msgPL.provide_liquidity.assets);
 
     let response = await provideLiquidity(cl, toPairAddr, msgPL);
-
+    console.log("provide: ", response);
     return response
 }
 
@@ -66,7 +70,7 @@ async function assetsInfo(cl: Client, pairAddr: string, msg: Object) {
 
 async function isExistsPair(cl: Client, assetInfo: any) {
     try {
-        await queryContract(cl.terra, terraSwapFactoryAddr, {"pair": {"asset_infos": assetInfo.asset_infos}});
+        await queryContract(cl.terra, astroportFactoryAddr, {"pair": {"asset_infos": assetInfo.asset_infos}});
         return true
     } catch (e: any) {
         console.log(e.response);
@@ -83,15 +87,43 @@ async function createPair(cl: Client, assetsInfo: any) {
         });
 }
 
-async function msgProvideLiquidity(assetsInfo: any, withdrawResponse: Object) {
+function withdrawAssetsAmount(withdrawResponse: any) {
+    let parsedLog = JSON.parse(withdrawResponse.raw_log);
+    let eventsWD = parsedLog[0].events;
+
+    let attributes = []
+    for (let i=0; i<eventsWD.length; i++){
+        if (eventsWD[i]['type'] === "from_contract"){
+            attributes = eventsWD[i]['attributes'];
+            break;
+        }
+    }
+
+    let refundAsset = ""
+    for (let i=0; i<attributes.length; i++) {
+        if (attributes[i]["key"] == "refund_assets"){
+            refundAsset = attributes[i]["value"];
+            break;
+        }
+    }
+    let assetsAmount = refundAsset.split(",");
+    console.log("assetsAmount: ", assetsAmount);
+
+    assetsAmount[0] = assetsAmount[0].trim().replace(/(^\d+)(.+$)/i,'$1');
+    assetsAmount[1] = assetsAmount[1].trim().replace(/(^\d+)(.+$)/i,'$1');
+
+    return assetsAmount
+}
+
+function msgProvideLiquidity(assetsInfo: any, assetsAmount: any) {
     let msgPL = {
         "provide_liquidity": {
             "assets": [{
                 "info": {},
-                "amount": "100"
+                "amount": assetsAmount[0]
             }, {
                 "info": {},
-                "amount": "1000"
+                "amount": assetsAmount[1]
             }],
         }
     }
@@ -110,8 +142,12 @@ async function provideLiquidity(cl: Client, pairAddr: string, msg: any) {
             coins[`${asset.info.native_token.denom}`] = asset.amount;
         }
     })
-    console.log("coins: ", coins);
-    //return await executeContract(cl.terra, cl.wallet, pairAddr, msg, coins);
+
+    if (Object.keys(coins).length === 0) {
+        return await executeContract(cl.terra, cl.wallet, pairAddr, msg);
+    } else {
+        return await executeContract(cl.terra, cl.wallet, pairAddr, msg, coins);
+    }
 }
 
 export async function pairs(cl: Client, factoryAddr: string, msg: Object) {
@@ -119,12 +155,10 @@ export async function pairs(cl: Client, factoryAddr: string, msg: Object) {
 }
 
 async function main() {
-
-    //const cl = newClient(nodeURL, chainID, walletMnemonic);
     try {
-        await migrateLiquidity(terraSwapPairAddr, astroportPairAddr, "20");
+        await migrateLiquidity(terraSwapPairAddr, astroportPairAddr, "10");
     } catch (e: any) {
-        console.log(e.response);
+        console.log(e);
     }
 }
 main().catch(console.log);
