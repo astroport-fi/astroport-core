fd91e3dcd679ff7854f4be0983cd521dbee16abd
Eugene <we.danced.a.lot@gmail.com>
2021-11-08 12:47:11 +0200

    feat: remove canonical addr from router

--- contracts/router/src/state.rs
+++ contracts/router/src/state.rs
@@ -2,11 +2,11 @@ use cw_storage_plus::Item;
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
-use cosmwasm_std::CanonicalAddr;
+use cosmwasm_std::Addr;
 
 pub const CONFIG: Item<Config> = Item::new("config");
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 pub struct Config {
-    pub astroport_factory: CanonicalAddr,
+    pub astroport_factory: Addr,
 }
