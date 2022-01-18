395efbc32761f6cfec37b5bdb8c87fc9728c312f
Timofey Epanchintsev <epanchee@gmail.com>
2022-01-18 15:32:13 +0500

    feat(maker): unit test fixed

--- contracts/tokenomics/maker/src/testing.rs
+++ contracts/tokenomics/maker/src/testing.rs
@@ -45,7 +45,7 @@ fn proper_initialization() {
             rewards_enabled: false,
             pre_upgrade_blocks: 0,
             last_distribution_block: 0,
-            remainder_reward: None,
+            remainder_reward: Uint128::zero(),
             pre_upgrade_astro_amount: Uint128::zero(),
         }
     )
