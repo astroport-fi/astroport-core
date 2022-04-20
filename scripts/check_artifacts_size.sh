2e2b7abedb1f9bc4364f1a9cff1df253a24866da
P-Yevhenii <py@atticlab.net>
2022-04-20 13:16:31 +0300

    added opt-level = 'z'

---
+++ scripts/check_artifacts_size.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+set -e
+set -o pipefail
+
+projectPath=$(cd "$(dirname "${0}")" && cd ../ && pwd)
+
+docker run -v "$projectPath":/code \
+  --mount type=volume,source="$(basename "$projectPath")_cache",target=/code/target \
+  --mount type=volume,source=registry_cache,target=/usr/local/cargo/registry \
+  cosmwasm/workspace-optimizer:0.12.6
+
+
+maximum_size=590
+
+for artifact in artifacts/*.wasm; do
+  artifactsize=$(du -k "$artifact" | cut -f 1)
+  if [ "$artifactsize" -gt $maximum_size ]; then
+    echo "Artifact file size exceeded: $artifact"
+    echo "Artifact size: $artifactsize"
+    echo "Max size: $maximum_size"
+    exit 1
+  fi
+done
