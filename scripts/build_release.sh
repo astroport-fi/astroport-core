0cc4fcf1cafe113e2b0ba416d20618fadf856e38
Maks Koval <maksk7742@gmail.com>
2022-05-03 14:06:12 +0300

    Shrink generator contract size

--- scripts/build_release.sh
+++ scripts/build_release.sh
@@ -8,4 +8,4 @@ projectPath=$(cd "$(dirname "${0}")" && cd ../ && pwd)
 docker run --rm -v "$projectPath":/code \
   --mount type=volume,source="$(basename "$projectPath")_cache",target=/code/target \
   --mount type=volume,source=registry_cache,target=/usr/local/cargo/registry \
-  cosmwasm/workspace-optimizer:0.12.5
+  cosmwasm/workspace-optimizer:0.12.6
