56a6b13f0658a683f1cfae1f60c66351e20d9e2c
P-Yevhenii <py@atticlab.net>
2022-06-08 02:36:08 +0300

    fixed astro package

--- packages/astroport/src/testing.rs
+++ packages/astroport/src/testing.rs
@@ -174,11 +174,6 @@ fn test_asset() {
         ],
     )]);
 
-    deps.querier.with_tax(
-        Decimal::percent(1),
-        &[(&"uusd".to_string(), &Uint128::new(1000000u128))],
-    );
-
     let token_asset = Asset {
         amount: Uint128::new(123123u128),
         info: AssetInfo::Token {
@@ -343,12 +338,12 @@ fn test_decimal_checked_ops() {
         let dec = Decimal::from_ratio(i, 1u128);
         assert_eq!(
             dec * Uint128::new(i),
-            dec.checked_mul(Uint128::new(i)).unwrap()
+            dec.checked_mul_uint128(Uint128::from(i)).unwrap()
         );
     }
     assert!(
         Decimal::from_ratio(Uint128::MAX, Uint128::from(10u128.pow(18u32)))
-            .checked_mul(Uint128::from(10u128.pow(18u32) + 1))
+            .checked_mul(Decimal::new(Uint128::from(10u128.pow(18u32) + 1u128)))
             .is_err()
     );
 }
