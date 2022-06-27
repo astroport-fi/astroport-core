8aeef8a2230b9bdf3d2f41d3dba29db644f841b2
P-Yevhenii <py@atticlab.net>
2022-06-27 10:42:00 +0300

    added console print

--- scripts/slack_notification.ts
+++ scripts/slack_notification.ts
@@ -6,11 +6,17 @@ const MY_SLACK_WEBHOOK_URL = process.env.SLACK_WEBHOOK_URL
 const slack = SlackNotify(MY_SLACK_WEBHOOK_URL);
 
 export async function sendNotification(name: string, msg: string, stack: string | undefined) {
-    slack.alert({
-        text: name,
-        fields: {
-            'message': msg,
-            'stack': stack
-        }
-    });
+    if (process.env.SLACK_WEBHOOK_URL!) {
+        slack.alert({
+            text: name,
+            fields: {
+                'message': msg,
+                'stack': stack
+            }
+        });
+    } else {
+        console.error(`${name} - ${msg} \n${stack}`)
+    }
 }
+
+sendNotification("hello", "message", "some info").catch(console.log)
