dba2ee0551826fa3089d061c8b04875ffa6d0082
P-Yevhenii <py@atticlab.net>
2022-06-29 02:57:58 +0300

    fix multitest script

--- scripts/CI_scripts/multitest.sh
+++ scripts/CI_scripts/multitest.sh
@@ -7,14 +7,14 @@ projectPath=$(cd "$(dirname "${0}")" && pwd)
 if [ ! -d "$projectPath"/astroport-core ];
 then
 	git clone --branch feat/slack_notification https://github.com/astroport-fi/astroport-core.git "$projectPath"/astroport-core
-	if ! cd "$projectPath"/astroport-core && cargo update && cargo test;
+	if ! (cd "$projectPath"/astroport-core && cargo update && cargo test);
   then
     cd "$projectPath"/astroport-core/scripts && node --loader ts-node/esm slack_notification.ts
   fi
 else
   git fetch
 
-  if ! cd "$projectPath"/astroport-core && cargo update && cargo test;
+  if ! (cd "$projectPath"/astroport-core && cargo update && cargo test);
   then
     cd "$projectPath"/astroport-core/scripts && node --loader ts-node/esm slack_notification.ts
   fi
@@ -24,14 +24,14 @@ if [ ! -d "$projectPath"/astroport-governance ];
 then
 	git clone --branch main https://github.com/astroport-fi/astroport-governance.git "$projectPath"/astroport-governance
 
-	if ! cd "$projectPath"/astroport-governance && cargo update && cargo test;
+	if ! (cd "$projectPath"/astroport-governance && cargo update && cargo test);
   then
     cd "$projectPath"/astroport-core/scripts && node --loader ts-node/esm slack_notification.ts
   fi
 else
   git fetch
 
-  if ! cd "$projectPath"/astroport-governance && cargo update && cargo test;
+  if ! (cd "$projectPath"/astroport-governance && cargo update && cargo test);
   then
     cd "$projectPath"/astroport-core/scripts && node --loader ts-node/esm slack_notification.ts
   fi
@@ -40,13 +40,13 @@ fi
 if [ ! -d "$projectPath"/astroport-bootstrapping ];
 then
 	git clone --branch main https://github.com/astroport-fi/astroport-bootstrapping.git "$projectPath"/astroport-bootstrapping
-	if ! cd "$projectPath"/astroport-bootstrapping && cargo update && cargo test;
+	if ! (cd "$projectPath"/astroport-bootstrapping && cargo update && cargo test);
   then
     cd "$projectPath"/astroport-core/scripts && node --loader ts-node/esm slack_notification.ts
   fi
 else
   git fetch
-  if ! cd "$projectPath"/astroport-bootstrapping && cargo update && cargo test;
+  if ! (cd "$projectPath"/astroport-bootstrapping && cargo update && cargo test);
   then
     cd "$projectPath"/astroport-core/scripts && node --loader ts-node/esm slack_notification.ts
   fi
