11512708faf81b9de549e9143e1c1bc27037779b
Sergey Shadoy <shadoysv@yandex.ru>
2022-07-01 10:45:01 +0300

    start type checking for generator proxy template

--- templates/generator_proxy_template/src/state.rs
+++ templates/generator_proxy_template/src/state.rs
@@ -5,21 +5,21 @@ use cosmwasm_std::Addr;
 use cw_storage_plus::Item;
 
 /// ## Description
-/// This structure stores the main params for the generator_proxy_template contract.
+/// This structure holds the main parameters of the generator_proxy_template contract.
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 pub struct Config {
     /// The Generator contract address
     pub generator_contract_addr: Addr,
-    /// The target Astroprot pair contract address
+    /// The target Astroport pair contract address
     pub pair_addr: Addr,
     /// The contract address for the Astroport LP token associated with pair_addr
     pub lp_token_addr: Addr,
-    /// The reward contract address (3rd party staking contract)
+    /// The 3rd party reward contract address
     pub reward_contract_addr: Addr,
     /// The 3rd party reward token
     pub reward_token_addr: Addr,
 }
 
 /// ## Description
-/// Stores config at the given key
+/// Stores the contract config at the given key
 pub const CONFIG: Item<Config> = Item::new("config");
