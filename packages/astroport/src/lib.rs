329889cebdf81e437a40a6ad47968dceafbb00a4
P-Yevhenii <py@atticlab.net>
2022-10-03 10:50:09 +0300

    refactor code

--- packages/astroport/src/lib.rs
+++ packages/astroport/src/lib.rs
@@ -33,16 +33,10 @@ mod decimal_checked_ops {
     use cosmwasm_std::{Decimal, Fraction, OverflowError, Uint128, Uint256};
     use std::convert::TryInto;
     pub trait DecimalCheckedOps {
-        fn checked_add(self, other: Decimal) -> Result<Decimal, OverflowError>;
         fn astro_checked_mul(self, other: Uint128) -> Result<Uint128, OverflowError>;
     }
 
     impl DecimalCheckedOps for Decimal {
-        fn checked_add(self, other: Decimal) -> Result<Decimal, OverflowError> {
-            self.numerator()
-                .checked_add(other.numerator())
-                .map(|_| self + other)
-        }
         fn astro_checked_mul(self, other: Uint128) -> Result<Uint128, OverflowError> {
             if self.is_zero() || other.is_zero() {
                 return Ok(Uint128::zero());
