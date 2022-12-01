f4b5441769c88f5cafd333aba17087deaf83b8d5
P-Yevhenii <py@atticlab.net>
2022-12-01 16:50:07 +0200

    refactor code

--- templates/generator_proxy_template/src/contract.rs
+++ templates/generator_proxy_template/src/contract.rs
@@ -82,7 +82,7 @@ pub fn execute(
 ) -> Result<Response, ContractError> {
     match msg {
         ExecuteMsg::Receive(msg) => receive_cw20(deps, env, info, msg),
-        ExecuteMsg::UpdateRewards {} => update_rewards(deps),
+        ExecuteMsg::UpdateRewards {} => update_rewards(deps, info),
         ExecuteMsg::SendRewards { account, amount } => send_rewards(deps, info, account, amount),
         ExecuteMsg::Withdraw { account, amount } => withdraw(deps, env, info, account, amount),
         ExecuteMsg::EmergencyWithdraw { account, amount } => {
