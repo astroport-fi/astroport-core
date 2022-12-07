b43d412cf0ecf18b0798b346a617365f7be34141
P-Yevhenii <86720500+P-Yevhenii@users.noreply.github.com>
2022-12-07 10:57:44 +0100

    feat(astroport_package): allow native asset to be in any case (main) (#15)

--- packages/astroport/src/asset.rs
+++ packages/astroport/src/asset.rs
@@ -223,14 +223,7 @@ impl AssetInfo {
             AssetInfo::Token { contract_addr } => {
                 addr_validate_to_lower(api, contract_addr)?;
             }
-            AssetInfo::NativeToken { denom } => {
-                if !denom.starts_with("ibc/") && denom != &denom.to_lowercase() {
-                    return Err(StdError::generic_err(format!(
-                        "Non-IBC token denom {} should be lowercase",
-                        denom
-                    )));
-                }
-            }
+            _ => {}
         }
         Ok(())
     }
