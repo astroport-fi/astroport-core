e2dd602758edee3992d60d0d75881257e9e6d332
P-Yevhenii <86720500+P-Yevhenii@users.noreply.github.com>
2023-03-17 13:34:46 +0100

    feat(contract): native coin wrapper

---
+++ contracts/periphery/native-coin-wrapper/src/error.rs
@@ -0,0 +1,18 @@
+use cosmwasm_std::StdError;
+use cw_utils::PaymentError;
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum ContractError {
+    #[error("{0}")]
+    Std(#[from] StdError),
+
+    #[error("{0}")]
+    PaymentError(#[from] PaymentError),
+
+    #[error("Unauthorized")]
+    Unauthorized {},
+
+    #[error("Failed to parse or process reply message")]
+    FailedToParseReply {},
+}
