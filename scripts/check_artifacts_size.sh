11e7a81d4b18a40bed916177061a549633e02b1b
Sergei Shadoy <shadoysv@gmail.com>
2023-05-22 17:54:45 +0300

    Increase maximum allowed wasm file size

--- scripts/check_artifacts_size.sh
+++ scripts/check_artifacts_size.sh
@@ -10,8 +10,9 @@ docker run -v "$projectPath":/code \
   --mount type=volume,source=registry_cache,target=/usr/local/cargo/registry \
   cosmwasm/workspace-optimizer:0.12.13
 
-
-maximum_size=700
+# terra: https://github.com/terra-money/wasmd/blob/2308975f45eac299bdf246737674482eaa51051c/x/wasm/types/validation.go#L12
+# injective: https://github.com/InjectiveLabs/wasmd/blob/e087f275712b5f0a798791495dee0e453d67cad3/x/wasm/types/validation.go#L19
+maximum_size=800
 
 for artifact in artifacts/*.wasm; do
   artifactsize=$(du -k "$artifact" | cut -f 1)
