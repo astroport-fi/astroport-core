1d078b2184debdeab20b00364c8672cb62ac6e48
Timofey <5527315+epanchee@users.noreply.github.com>
2023-07-06 12:22:09 +0300

    fix(PCL): fixed rounding errors in withdraw endpoint
    (cherry picked from commit 9df4d4a072df996f0e66c6ccd433ef698de1a0fc)

--- contracts/pair_concentrated/src/queries.rs
+++ contracts/pair_concentrated/src/queries.rs
@@ -101,7 +101,8 @@ fn query_share(deps: Deps, amount: Uint128) -> Result<Vec<Asset>, ContractError>
         &precisions,
     )?;
     let total_share = query_supply(&deps.querier, &config.pair_info.liquidity_token)?;
-    let refund_assets = get_share_in_assets(&pools, amount, total_share);
+    let refund_assets =
+        get_share_in_assets(&pools, amount.saturating_sub(Uint128::one()), total_share);
 
     let refund_assets = refund_assets
         .into_iter()
