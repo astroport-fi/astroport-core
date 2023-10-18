ec8e3dd2f9ae366f0e19c6c8f2fc16a6e240a4fd
Sturdy <91910406+apollo-sturdy@users.noreply.github.com>
2023-10-18 12:06:31 +0200

    fix: Allow migrating from 1.3.0 and 1.3.1

--- contracts/pair_xyk_sale_tax/src/contract.rs
+++ contracts/pair_xyk_sale_tax/src/contract.rs
@@ -1324,11 +1324,12 @@ pub fn migrate(deps: DepsMut, _env: Env, msg: MigrateMsg) -> Result<Response, Co
 
     // If migrating from default xyk pair, we must make some state changes
     if contract_version.contract == "astroport-pair" {
-        if contract_version.version != "1.4.0" {
-            return Err(StdError::generic_err(
-                "Incompatible version of astroport-pair. Only 1.4.0 supported.",
+        match contract_version.version.as_str() {
+            "1.3.0" | "1.3.1" | "1.4.0" => {}
+            _ => return Err(StdError::generic_err(
+                "Incompatible version of astroport-pair. Only 1.3.0, 1.3.1, and 1.4.0 supported.",
             )
-            .into());
+            .into()),
         }
 
         // Read old config
