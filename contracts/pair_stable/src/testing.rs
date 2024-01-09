f74eeefbb3c02630e886596ca188dc307c057c2e
Timofey <5527315+epanchee@users.noreply.github.com>
2024-01-09 13:15:14 +0400

    fix(PCL): optimize internal stableswap function and df/dd

--- contracts/pair_stable/src/testing.rs
+++ contracts/pair_stable/src/testing.rs
@@ -1367,7 +1367,7 @@ proptest! {
         let diff = (sim_result as i128 - result.return_amount.u128() as i128).abs();
 
         assert!(
-            diff <= 10,
+            diff <= 20,
             "result={}, sim_result={}, amp={}, amount_in={}, balance_in={}, balance_out={}, diff={}",
             result.return_amount,
             sim_result,
