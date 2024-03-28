554dcae3ed7312425fccb75322030a885100e9ef
Timofey <5527315+epanchee@users.noreply.github.com>
2024-03-28 17:35:16 +0100

    remove obsolete and unused dev scripts

--- scripts/build_app.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-projectPath=$(cd "$(dirname "${0}")" && cd ../ && pwd)
-
-cd "$projectPath/scripts" && node --loader ts-node/esm deploy_core.ts
-cd "$projectPath/scripts" && node --loader ts-node/esm deploy_pools.ts
