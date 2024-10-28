b3728b72dd4085fccb75de6e3befd8a3d122aac6
Timofey <5527315+epanchee@users.noreply.github.com>
2024-10-28 14:39:52 +0400

    feat: downgrade dependencies to cosmwasm_1_0 capability for Sei
    Most of the important Astroport contracts are excluded.
    Although some basic contracts like incentives and coin registry are still able to work without modern cw capabilities.

--- scripts/build_release.sh
+++ scripts/build_release.sh
@@ -8,4 +8,4 @@ projectPath=$(cd "$(dirname "${0}")" && cd ../ && pwd)
 docker run --rm -v "$projectPath":/code \
   --mount type=volume,source="$(basename "$projectPath")_cache",target=/target \
   --mount type=volume,source=registry_cache,target=/usr/local/cargo/registry \
-  ghcr.io/astroport-fi/rust-optimizer:v0.15.1-astroport
+  cosmwasm/workspace-optimizer:0.15.1
