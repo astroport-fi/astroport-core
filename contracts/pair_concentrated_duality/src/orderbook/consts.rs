93c50d8d3e26e032cb7b08b834c4aa6ad6f63377
Timofey <5527315+epanchee@users.noreply.github.com>
2025-03-13 13:11:29 -0300

    fix: constant docstring

--- contracts/pair_concentrated_duality/src/orderbook/consts.rs
+++ contracts/pair_concentrated_duality/src/orderbook/consts.rs
@@ -9,7 +9,7 @@ use cosmwasm_std::Decimal;
 /// - 30 orders incur ~6.15M gas on swap, ~6.08M on provide and ~6.14M on withdraw.
 pub const ORDERS_NUMBER_LIMITS: RangeInclusive<u8> = 1..=15;
 
-/// Min liquidity percent for orders to be placed in the orderbook. (0.01%)
+/// Min liquidity percent for orders to be placed in the orderbook. (1%)
 pub const MIN_LIQUIDITY_PERCENT: Decimal = Decimal::raw(1e16 as u128);
 /// Max liquidity percent for orders to be placed in the orderbook. (50%)
 pub const MAX_LIQUIDITY_PERCENT: Decimal = Decimal::raw(5e17 as u128);
