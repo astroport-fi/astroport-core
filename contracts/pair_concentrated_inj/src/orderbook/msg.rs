da3673ad736693638db78712139890073d806db1
Timofey <5527315+epanchee@users.noreply.github.com>
2025-08-27 13:26:57 +0300

    chore: remove all injective-related contracts

--- contracts/pair_concentrated_inj/src/orderbook/msg.rs
+++
@@ -1,9 +0,0 @@
-use cosmwasm_schema::cw_serde;
-
-#[cw_serde]
-/// SudoMsg layout is defined within Injective core. We can not change it.
-pub enum SudoMsg {
-    BeginBlocker {},
-    Deactivate {},
-    Deregister {},
-}
